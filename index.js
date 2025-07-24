879821e0e5765278dff318491cd5b201b42beddd
parisa zarei <parisa@idmelon.com>
2025-07-24 18:08:41 +0330

    feat: check Redis connectivity

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 require('dotenv').config();
 const app = require('./src/app');
+const redisClient = require('./src/configs/redis.config.js');
+const PORT = process.env.PORT || 3000;
 
-function bootstrap() {
-  const PORT = process.env.PORT || 3000;
 
-  app.listen(PORT, () => {
+async function connectRedis() {
+  try {
+    await redisClient.ping();
+    console.log('✅ Redis ping successfully');
+  } catch (err) {
+    console.error('❌ Redis  ping failed:', err);
+    process.exit(1);
+  }
+}
+
+async function bootstrap() {
+  await connectRedis();
+
+  return new Promise((resolve) => {
+    app.listen(PORT, () => {
       console.log(`🚀 Dekamond OTP auth Server is running on port ${PORT}`);
+      resolve(); 
+    });
   });
 }
 
 bootstrap();
+
