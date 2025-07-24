e0971d3ef1523022abbcb377208b5d96e71c87c1
parisa zarei <parisa@idmelon.com>
2025-07-24 17:48:39 +0330

    feat: add Redis config

--- src/configs/redis.config.js
+++ src/configs/redis.config.js
@@ -1,8 +1,7 @@
-require('dotenv').config();
 const Redis = require('ioredis');
 
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST || 'redis',
+  host: process.env.REDIS_HOST || 'localhost',
   port: process.env.REDIS_PORT || 6379
 });
 
