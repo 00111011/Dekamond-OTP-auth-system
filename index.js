06e2d93c4c64033b53eea70418046609d67ab54b
parisa zarei <parisa@idmelon.com>
2025-07-26 18:10:52 +0330

    feat: check Postgres connectivity

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 require('dotenv').config();
 const app = require('./src/app');
 const redisClient = require('./src/configs/redis.config.js');
+const db = require('./src/configs/postgres.config.js');
 const PORT = process.env.PORT || 3000;
+
+async function connectPostgres() {
+  try {
+    await db.authenticate();
+    console.log('✅ PostgreSQL connected successfully');
+  } catch (err) {
+    console.error('❌ Error connecting to PostgreSQL:', err);
+    process.exit(1);
+  }
+}
 
 
 async function connectRedis() {
@@ -16,7 +27,7 @@ async function connectRedis() {
 
 async function bootstrap() {
   await connectRedis();
-
+  await connectPostgres();
   return new Promise((resolve) => {
     app.listen(PORT, () => {
       console.log(`🚀 Dekamond OTP auth Server is running on port ${PORT}`);
