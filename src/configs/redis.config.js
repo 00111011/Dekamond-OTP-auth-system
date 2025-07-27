5b361da950327814d58fd8739d08cbd3fda0772d
parisa zarei <parisa@idmelon.com>
2025-07-27 20:20:16 +0330

    fix(redis): improve connection configuration

--- src/configs/redis.config.js
+++ src/configs/redis.config.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const Redis = require('ioredis');
 
 const redisClient = new Redis({
-  host: process.env.REDIS_HOST || 'localhost',
+  host: process.env.REDIS_HOST || 'redis',
   port: process.env.REDIS_PORT || 6379
 });
 
